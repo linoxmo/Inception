0420d8e14aa8b42b8706f1c629664056c1824380
tmagoudi <tmagoudi@f1br6s16.paris.42.school>
2026-08-25 14:52:23 +0200

    error on maridab correction

--- srcs/MariaDb/mariadb.sh
+++ srcs/MariaDb/mariadb.sh
@@ -9,13 +9,13 @@ chown -R mysql:mysql /var/lib/mysql
 
 
 
-mysqld_safe --datadir=/var/lib/mysql &
+mariadbd-safe --datadir=/var/lib/mysql &
 
-until mysqladmin ping --silent; do
+until mysqladmin -h 127.0.0.1 ping --silent; do
     sleep 1
 done
 
-mysql -h 127.0.0.1 -u root << EOF
+mariadb -u root << EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
@@ -23,6 +23,6 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin -h 127.0.0.1 -u root shutdown
+mariadb-admin -u root -p "${MYSQL_ROOT_PASSWORD}" shutdown
 
-exec mysqld --user=mysql --console
+exec mariadb --user=mysql --console
