6324abe0c03ca081d9cc4dd73561b45c4bb6036b
tmagoudi <tmagoudi@f6r11s6.paris.42.school>
2026-08-27 13:59:49 +0200

    Mariadb error

--- srcs/MariaDb/mariadb.sh
+++ srcs/MariaDb/mariadb.sh
@@ -3,26 +3,55 @@
 set -e
 
 mkdir -p /run/mysqld
-
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 
+    mariadb-install-db \
+        --user=mysql \
+        --datadir=/var/lib/mysql \
+        --skip-test-db
 
-mariadbd-safe --datadir=/var/lib/mysql &
+    mariadbd \
+        --user=mysql \
+        --datadir=/var/lib/mysql \
+        --skip-networking &
 
-until mysqladmin -h 127.0.0.1 ping --silent; do
-    sleep 1
-done
+    for i in $(seq 1 30); do
+        if mariadb-admin \
+            --socket=/run/mysqld/mysqld.sock \
+            ping >/dev/null 2>&1
+        then
+            break
+        fi
+        sleep 1
+    done
 
-mariadb -u root << EOF
+    mariadb \
+        --socket=/run/mysqld/mysqld.sock \
+        -u root << SQL
+
+ALTER USER 'root'@'localhost'
+IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%'
+IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.*
+TO '${MYSQL_USER}'@'%';
+
 FLUSH PRIVILEGES;
-EOF
 
-mariadb-admin -u root -p "${MYSQL_ROOT_PASSWORD}" shutdown
+SQL
 
-exec mariadb --user=mysql --console
+    mariadb-admin \
+        --socket=/run/mysqld/mysqld.sock \
+        -u root \
+        -p"${MYSQL_ROOT_PASSWORD}" \
+        shutdown
+fi
+
+exec mariadbd --user=mysql --console
